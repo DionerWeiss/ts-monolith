199edee92b0454b802580baa484183e24ea6f92e
dioner <dionerweiss@gmail.com>
2023-03-14 23:06:07 -0300

    create a factory to ProductAdmFacade

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
+import { ProductAdmFacadeFactory } from "../factory/facade.factory";
 import { ProductModel } from "../repository/product.model";
-import { ProductRepository } from "../repository/product.repository";
-import { AddProductUseCase } from "../usecase/add-product/add-product.usecase";
-import { ProductAdmFacade } from "./product-adm.facade";
 
 describe('ProductAdmFacade', () => {
   let sequelize: Sequelize
@@ -24,12 +22,7 @@ describe('ProductAdmFacade', () => {
   })
 
   it('should create a product', async () => {
-    const productRepository = new ProductRepository()
-    const addProductUseCase = new AddProductUseCase(productRepository)
-    const productFacade = new ProductAdmFacade({
-      addUseCase: addProductUseCase,
-      stockUseCase: undefined
-    })
+    const productFacade = ProductAdmFacadeFactory.create();
 
     const input = {
       id: "1",

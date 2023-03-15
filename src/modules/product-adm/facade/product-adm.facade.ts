c08867c81314afc0f25b3b0d7a2aea7657b1f5da
dioner <dionerweiss@gmail.com>
2023-03-14 22:57:42 -0300

    create ProductAdmFacade

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,25 @@
+import { UseCaseInterface } from "../../@shared/usecase/use-case.interface";
+import { AddProductFacadeInputDto, CheckStockFacadeInputDto, CheckStockFacadeOutputDto, ProductAdmFacadeInterface } from "./product-adm.facade.interface";
+
+export interface UseCasesProps {
+  addUseCase: UseCaseInterface;
+  stockUseCase: UseCaseInterface;
+}
+
+export class ProductAdmFacade implements ProductAdmFacadeInterface {
+  private _addUsecase: UseCaseInterface;
+  private _checkStockUsecase: UseCaseInterface;
+
+  constructor(usecasesProps: UseCasesProps) {
+    this._addUsecase = usecasesProps.addUseCase;
+    this._checkStockUsecase = usecasesProps.stockUseCase;
+  }
+
+  async addProduct(input: AddProductFacadeInputDto): Promise<void> {
+    return this._addUsecase.execute(input);
+  }
+  
+  async checkStock(input: CheckStockFacadeInputDto): Promise<CheckStockFacadeOutputDto> {
+    throw new Error("Method not implemented.");
+  }
+}
